691f07a51d8612c3243eb9f6e336f9adb8dca59d
Cesar Garcia <cesar.garcia@citricosoft.com>
2021-11-25 14:14:24 -0600

    creacion de modelos

---
+++ app/src/app/clientes/cliente.ts
@@ -0,0 +1,8 @@
+export interface Cliente {
+    id: number;
+    nombre: string;
+    apellido: string;
+    telefono: string;
+    correo: string;
+    nit: number;
+}
---
+++ app/src/app/ordenes/orden.ts
@@ -0,0 +1,7 @@
+export interface Orden {
+    idProducto: number;
+    idCliente: number;
+    idOrden: number;
+    cantidad: number;
+    fecha: number;
+}
